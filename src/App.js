484c6ff96d20b28c46ff1b6afe56190fa8c130f0
narendra bisht <narendraktw@gmail.com>
2020-07-11 00:41:52 +0530

    hook implemented

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
+import HookCounter from './components/HookCounter';
 // import Greet from './components/Greet'
-// import Welcome from './components/Welcome'
+ //import Welcome from './components/Welcome'
 // import Message from './components/Message'
 //import Hello from './components/Hello'
 //import Counter from './components/Counter';
@@ -9,12 +10,12 @@ import './App.css';
 //import ClassClick from './components/ClassClick'
 //import EventBind from './components/EventBind';
 //import ParentComponent from './components/ParentComponent';
-import NameList from './components/NameList';
+//import NameList from './components/NameList';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <NameList/>
+        <HookCounter/>
       </div>
     )
 }}
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react'
 class Welcome extends Component {
     render(){
         //return <h1>Hello Bisht</h1>
-    return <h1>Welcom {this.props.name} a.k.a {this.props.heroname}</h1>
+    return <h1>Welcome {this.props.name} a.k.a {this.props.heroName}</h1>
     }
 }
 
